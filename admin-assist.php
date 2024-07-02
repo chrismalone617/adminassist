fe1407ad83bdec828472fb3e36ffed3f4e03a15c
pressnest <136358711+pressnest@users.noreply.github.com>
2024-07-01 19:24:48 -0500

    add icon

--- admin-assist.php
+++ admin-assist.php
@@ -14,114 +14,115 @@
  * Website:           
  */
 
- 
- // Exit if accessed directly
- if ( ! defined( 'ABSPATH' ) ) {
-     exit;
- }
- 
- // Create the database table on plugin activation
- function adminassist_create_table() {
-     global $wpdb;
-     $table_name = $wpdb->prefix . 'adminassist';
-     $charset_collate = $wpdb->get_charset_collate();
- 
-     $sql = "CREATE TABLE $table_name (
-         id mediumint(9) NOT NULL AUTO_INCREMENT,
-         looker_url varchar(255) NOT NULL,
-         hide_widget varchar(3) NOT NULL,
-         PRIMARY KEY  (id)
-     ) $charset_collate;";
- 
-     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-     dbDelta( $sql );
- }
- 
- register_activation_hook( __FILE__, 'adminassist_create_table' );
- 
- // Delete the database table on plugin uninstall
- function adminassist_delete_table() {
-     global $wpdb;
-     $table_name = $wpdb->prefix . 'adminassist';
- 
-     $sql = "DROP TABLE IF EXISTS $table_name;";
-     $wpdb->query( $sql );
- }
- 
- register_uninstall_hook( __FILE__, 'adminassist_delete_table' );
- 
- // Add the settings section and fields
- function adminassist_add_settings() {
-     add_settings_section(
-         'adminassist_settings_section',
-         'Google Analytics Report Details',
-         'adminassist_settings_section_callback',
-         'general'
-     );
- 
-     add_settings_field(
-         'looker_url',
-         'Looker Studio Report URL',
-         'adminassist_looker_url_callback',
-         'general',
-         'adminassist_settings_section'
-     );
- 
-     add_settings_field(
-         'hide_widget',
-         'Hide the dashboard widget?',
-         'adminassist_hide_widget_callback',
-         'general',
-         'adminassist_settings_section'
-     );
- 
-     register_setting( 'general', 'looker_url' );
-     register_setting( 'general', 'hide_widget' );
- }
- 
- add_action( 'admin_init', 'adminassist_add_settings' );
- 
- function adminassist_settings_section_callback() {
-     echo '<hr>';
- }
- 
- function adminassist_looker_url_callback() {
-     $looker_url = get_option( 'looker_url', '' );
-     echo '<input type="url" id="looker_url" name="looker_url" value="' . esc_attr( $looker_url ) . '" class="regular-text ltr" />';
- }
- 
- function adminassist_hide_widget_callback() {
-     $hide_widget = get_option( 'hide_widget', 'no' );
-     echo '<select id="hide_widget" name="hide_widget">
-             <option value="no" ' . selected( $hide_widget, 'no', false ) . '>No</option>
-             <option value="yes" ' . selected( $hide_widget, 'yes', false ) . '>Yes</option>
-           </select>';
- }
- 
- // Save the settings in the custom database table
- function adminassist_save_settings() {
-     global $wpdb;
-     $table_name = $wpdb->prefix . 'adminassist';
-     
-     $looker_url = get_option( 'looker_url' );
-     $hide_widget = get_option( 'hide_widget' );
- 
-     $wpdb->replace(
-         $table_name,
-         array(
-             'id' => 1,
-             'looker_url' => $looker_url,
-             'hide_widget' => $hide_widget
-         ),
-         array(
-             '%d',
-             '%s',
-             '%s'
-         )
-     );
- }
- 
- add_action( 'update_option_looker_url', 'adminassist_save_settings' );
- add_action( 'update_option_hide_widget', 'adminassist_save_settings' );
- ?>
- 
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Create the database table on plugin activation
+function adminassist_create_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'adminassist';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        looker_url varchar(255) NOT NULL,
+        hide_widget varchar(3) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+register_activation_hook( __FILE__, 'adminassist_create_table' );
+
+// Delete the database table on plugin uninstall
+function adminassist_delete_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'adminassist';
+
+    $sql = "DROP TABLE IF EXISTS $table_name;";
+    $wpdb->query( $sql );
+}
+
+register_uninstall_hook( __FILE__, 'adminassist_delete_table' );
+
+// Add the settings section and fields
+function adminassist_add_settings() {
+    add_settings_section(
+        'adminassist_settings_section',
+        'Google Analytics Report Details',
+        'adminassist_settings_section_callback',
+        'general'
+    );
+
+    add_settings_field(
+        'looker_url',
+        'Looker Studio Report URL',
+        'adminassist_looker_url_callback',
+        'general',
+        'adminassist_settings_section'
+    );
+
+    add_settings_field(
+        'hide_widget',
+        'Hide the dashboard widget?',
+        'adminassist_hide_widget_callback',
+        'general',
+        'adminassist_settings_section'
+    );
+
+    register_setting( 'general', 'looker_url' );
+    register_setting( 'general', 'hide_widget' );
+}
+
+add_action( 'admin_init', 'adminassist_add_settings' );
+
+function adminassist_settings_section_callback() {
+    echo '<hr>';
+    echo '<?xml version="1.0" ?><!DOCTYPE svg  PUBLIC "-//W3C//DTD SVG 1.1//EN"  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg height="24px" style="enable-background:new 0 0 512 512; vertical-align: middle; margin-right: 10px;" version="1.1" viewBox="0 0 512 512" width="24px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="_x31_08-analytics_x2C__google_analytics_x2C__google"><g><g><g><path d="M330.564,166.28H181.438c-8.232,0-14.912,6.699-14.912,14.956v299.06      c0,8.251,6.68,14.954,14.912,14.954h149.127c8.23,0,14.91-6.703,14.91-14.954v-299.06      C345.475,172.979,338.795,166.28,330.564,166.28L330.564,166.28z M330.564,166.28" style="fill:#FFC107;"/><path d="M181.438,315.813H32.313c-8.236,0-14.916,6.698-14.916,14.953v149.53      c0,8.251,6.68,14.954,14.916,14.954h149.125c8.234,0,14.914-6.703,14.914-14.954v-149.53      C196.352,322.511,189.672,315.813,181.438,315.813L181.438,315.813z M181.438,315.813" style="fill:#FFC107;"/><path d="M479.689,16.75H330.564c-8.236,0-14.916,6.698-14.916,14.958v448.588      c0,8.251,6.68,14.954,14.916,14.954h149.125c8.234,0,14.914-6.703,14.914-14.954V31.708      C494.604,23.448,487.924,16.75,479.689,16.75L479.689,16.75z M479.689,16.75" style="fill:#FFA000;"/></g></g></g></g><g id="Layer_1"/></svg>';
+    echo 'Google Analytics Report Details';
+}
+
+function adminassist_looker_url_callback() {
+    $looker_url = get_option( 'looker_url', '' );
+    echo '<input type="url" id="looker_url" name="looker_url" value="' . esc_attr( $looker_url ) . '" class="regular-text ltr" />';
+}
+
+function adminassist_hide_widget_callback() {
+    $hide_widget = get_option( 'hide_widget', 'no' );
+    echo '<select id="hide_widget" name="hide_widget">
+            <option value="no" ' . selected( $hide_widget, 'no', false ) . '>No</option>
+            <option value="yes" ' . selected( $hide_widget, 'yes', false ) . '>Yes</option>
+          </select>';
+}
+
+// Save the settings in the custom database table
+function adminassist_save_settings() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'adminassist';
+    
+    $looker_url = get_option( 'looker_url' );
+    $hide_widget = get_option( 'hide_widget' );
+
+    $wpdb->replace(
+        $table_name,
+        array(
+            'id' => 1,
+            'looker_url' => $looker_url,
+            'hide_widget' => $hide_widget
+        ),
+        array(
+            '%d',
+            '%s',
+            '%s'
+        )
+    );
+}
+
+add_action( 'update_option_looker_url', 'adminassist_save_settings' );
+add_action( 'update_option_hide_widget', 'adminassist_save_settings' );
+?>

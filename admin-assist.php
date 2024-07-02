8515070fe70b0de26a4e8f82d17858b3f00da174
pressnest <136358711+pressnest@users.noreply.github.com>
2024-07-01 19:41:25 -0500

    add css and widget

--- admin-assist.php
+++ admin-assist.php
@@ -125,4 +125,39 @@ function adminassist_save_settings() {
 
 add_action( 'update_option_looker_url', 'adminassist_save_settings' );
 add_action( 'update_option_hide_widget', 'adminassist_save_settings' );
+
+// Add the dashboard widget
+function adminassist_add_dashboard_widget() {
+    $hide_widget = get_option( 'hide_widget', 'no' );
+
+    if ( $hide_widget === 'yes' ) {
+        return; // Exit if the widget should be hidden
+    }
+
+    wp_add_dashboard_widget(
+        'adminassist_dashboard_widget', // Widget slug
+        '<div style="display: flex; align-items: center;">
+            <svg height="24px" style="enable-background:new 0 0 512 512; vertical-align: middle; margin-right: 10px;" version="1.1" viewBox="0 0 512 512" width="24px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="_x31_08-analytics_x2C__google_analytics_x2C__google"><g><g><g><path d="M330.564,166.28H181.438c-8.232,0-14.912,6.699-14.912,14.956v299.06 c0,8.251,6.68,14.954,14.912,14.954h149.127c8.23,0,14.91-6.703,14.91-14.954v-299.06 C345.475,172.979,338.795,166.28,330.564,166.28L330.564,166.28z M330.564,166.28" style="fill:#FFC107;"/><path d="M181.438,315.813H32.313c-8.236,0-14.916,6.698-14.916,14.953v149.53 c0,8.251,6.68,14.954,14.916,14.954h149.125c8.234,0,14.914-6.703,14.914-14.954v-149.53 C196.352,322.511,189.672,315.813,181.438,315.813L181.438,315.813z M181.438,315.813" style="fill:#FFC107;"/><path d="M479.689,16.75H330.564c-8.236,0-14.916,6.698-14.916,14.958v448.588 c0,8.251,6.68,14.954,14.916,14.954h149.125c8.234,0,14.914-6.703,14.914-14.954V31.708 C494.604,23.448,487.924,16.75,479.689,16.75L479.689,16.75z M479.689,16.75" style="fill:#FFA000;"/></g></g></g></g><g id="Layer_1"/></svg>
+            Google Analytics Report
+        </div>', // Widget title
+        'adminassist_dashboard_widget_content' // Callback function to display the content
+    );
+}
+
+add_action( 'wp_dashboard_setup', 'adminassist_add_dashboard_widget' );
+
+function adminassist_dashboard_widget_content() {
+    $looker_url = get_option( 'looker_url', '' );
+    ?>
+    <p>The Google Analytics report provides detailed insights into your website's performance, user behavior, and traffic sources. Utilize this data to enhance your online strategy and achieve your business goals.</p>
+    <a href="<?php echo esc_url( $looker_url ); ?>" class="button button-primary adminassist-view-report">View Analytics Report</a>
+    <?php
+}
+
+// Enqueue custom styles
+function adminassist_enqueue_dashboard_styles() {
+    wp_enqueue_style( 'adminassist_dashboard_styles', plugins_url( 'assets/style.css', __FILE__ ) );
+}
+
+add_action( 'admin_enqueue_scripts', 'adminassist_enqueue_dashboard_styles' );
 ?>
